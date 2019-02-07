21fd91376eb39608cbe1125aaccacdbc7ec83dcf
difa <masfuddifapratama@gmail.com>
2019-02-07 21:32:00 +0700

    versi 0.1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from './components/Table';
+import Form from './components/Form';
 
 class App extends Component {
+  state = {
+    characters: [
+      // {
+      //   'name': 'Bambang',
+      //   'pekerjaan': 'Mechanic'
+      // },
+      // {
+      //   'name': 'Bharatheon',
+      //   'pekerjaan': 'Petani'
+      // },
+      // {
+      //   'name': 'Ramsey',
+      //   'pekerjaan': 'Archer'
+      // },
+      // {
+      //   'name': 'John Snow',
+      //   'pekerjaan': 'Knight'
+      // }
+    ]
+  };
+
+  removeCharacter = index => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    });
+  }
+
+  handleSubmit = character => {
+    this.setState({characters: [...this.state.characters, character]});
+  }
+
   render() {
+    const { characters } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+        < Form handleSubmit={this.handleSubmit}/>
       </div>
     );
   }
